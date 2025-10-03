60b68a503ec035f897b75338aa512b9cfc44ed62
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2025-10-02 21:14:00 -0300

    Modificando layout dos comentários, decidi deixar a introdução completamente comentada e posteriormente faço os passos iniciiais novamente

--- Exercícios Java/TrêsExercícios.java
+++ Exercícios Java/TrêsExercícios.java
@@ -3,23 +3,23 @@ import java.util.Scanner; //Abrindo chamado para futuro uso do Scanner
 public class TrêsExercícios {
     public static void main(String[] args) {
 
-        // Objeto para ler informações do teclado
+         /* Objeto para ler informações do teclado
         Scanner teclado = new Scanner(System.in);
 
-        /* EXERCÍCIO 1 */
 
-        /* Introdução
-         * 
-         * Abaixo temos variáveis, int = número inteiro, double = número decimal e String = letras, palavras, etc.
-         * Quando colocamos --> Variável = alguma coisa   estamos declarando ela, nesse caso, não igualamos a 0, mas deixamos o usuário inserir o valor
-         * por meio do Scanner.
-         * 'teclado.nextInt/Double/Line'
-         */
+         Introdução
+          
+          Abaixo temos variáveis, int = número inteiro, double = número decimal e String = letras, palavras, etc.
+          Quando colocamos --> Variável = alguma coisa <-- estamos declarando ela, nesse caso, não igualamos a 0,
+          mas deixamos o usuário inserir o valor por meio do Scanner.
+
+          -- 'teclado.nextInt/Double/Line'
+        
         int numInteiro = teclado.nextInt();
         double numDecimal = teclado.nextDouble();
         String variavelNome  = teclado.nextLine();
 
-        //Após abrir o chamado para teclado, deve fecha-lo
-        teclado.close();
+        Após abrir o chamado para teclado, deve fecha-lo
+        teclado.close(); */
     }
 }
