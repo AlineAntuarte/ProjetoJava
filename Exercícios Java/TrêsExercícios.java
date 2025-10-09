2655d24f585a21f6f91100e71125bb660294287a
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2025-10-09 19:37:49 -0300

    Transformando exercício 2 em comentário

--- Exercícios Java/TrêsExercícios.java
+++ Exercícios Java/TrêsExercícios.java
@@ -65,29 +65,8 @@ public class TrêsExercícios {
 
         // -----------------------------------------------------------------------------------------------------------------
 
-        {
+      /*   {
             // Exercício 2
-
-            /*
-             * Elabore um fluxograma para um algoritmo que LÊ os salários por hora (em R$
-             * por hora) e as horas trabalhadas de três colaboradores de uma empresa e
-             * ESCREVE o pagamento de cada um deles, bem como o total pago pela empresa. Não
-             * utilize mais que quatro variáveis. A ordem de leitura dos dados de entrada
-             * deve ser:
-             * Salário do colaborador 1
-             * horas trabalhadas do colaborador 1
-             * salário do colaborador 2
-             * horas trabalhadas do colaborador 2
-             * salário do colaborador 3
-             * horas trabalhadas do colaborador 3.
-             * A ordem de escrita dos dados de saída deve ser:
-             * Pagamento do colaborador 1
-             * pagamento do colaborador 2
-             * pagamento do colaborador 3
-             * total pago pela empresa.
-             * Em seguida, execute um teste de mesa com a entrada 50, 8, 60, 7, 50, 7.5;
-             * A saída deve ser 400, 420, 375, 1195.
-             */
 
             System.out.println("Exercício 2 | Salário hora");
             Scanner teclado = new Scanner(System.in);
@@ -112,7 +91,9 @@ public class TrêsExercícios {
 
             teclado.close();
         }
+ */
         // -----------------------------------------------------------------------------------------------------------------
 
+        // Exercício 3
     }
 }
