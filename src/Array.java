7bfc2868bcb00a7a69d17aad68a19e82b9a847be
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2025-10-23 17:46:21 -0300

    Exercícios de Array

---
+++ src/Array.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Array{
+    public static void main(String[] args) {
+        Scanner usuario = new Scanner(System.in);
+
+        // Código
+        
+        usuario.close();
+    }
+}
